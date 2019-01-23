3cf51743b294928f4e4c5e4332b5b2864b4bc450
T0114413@ciber.local <T0114413@ciber.local>
2019-01-23 22:24:42 +0000

    Ficha 3 Acabada

--- FT01/ExA/Ficha_Trabalho_3/Program.cs
+++ FT01/ExA/Ficha_Trabalho_3/Program.cs
@@ -18,7 +18,7 @@ namespace Ficha_Trabalho_3
             Console.WriteLine("\nDiferença entre 2 horas: " + dt1.Hora.difEntre2Horas(dt2.Hora));
             Console.WriteLine("\n\tDiferença entre 2 anos: " + dt1.difEntre2anos(dt2));
 
-            Console.WriteLine("-----------------Pontos----------------");
+            Console.WriteLine("-----------------Pontos----------------------");
             Ponto pt1 = new Ponto(20, 10);
             Ponto pt2 = new Ponto(10, 5);
             Ponto pt3 = new Ponto(1, 1);
@@ -28,7 +28,7 @@ namespace Ficha_Trabalho_3
             Console.WriteLine("\nPonto 2:" + pt2.toString());
             Console.WriteLine("\nDistancia entre o Ponto 1 e o Ponto 2: " + pt1.distEntre2Pontos(pt2));
 
-            Console.WriteLine("-----------------Retas----------------");
+            Console.WriteLine("-----------------Retas-----------------------");
             Reta rt1 = new Reta(pt1, pt2);
             Reta rt2 = new Reta(pt3, pt4);
             Console.WriteLine("\nReta 1: " + rt1.toString());
